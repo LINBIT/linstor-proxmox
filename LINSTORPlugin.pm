b8f43147e0d7d5aff0eb8200e538c2103a969748
Lars Ellenberg <lars.ellenberg@linbit.com>
2018-09-28 15:09:52 +0200

    status "freespace" reporting: iterate over all storage pools
    As long as no storage pool is explicitly defined,
    with auto-place, you'll get "any" of them.
    
    So *if* there are more than one storage pools, the free/used/total
    reporting should iterate over all of them.

--- LINSTORPlugin.pm
+++ LINSTORPlugin.pm
@@ -334,23 +334,26 @@ sub status {
     my $controller = get_controller($scfg);
     my $nodename   = PVE::INotify::nodename();
 
-    my ( $total, $avail, $used );
+    my ( $total, $avail );
 
     my $json = decode_json_from_pipe(
 	    $LINSTOR, "--controllers=$controller", "-m",
 	    "storage-pool", "list", "-n", $nodename);
 
-    # we assume that in proxmox setups there is exactly one pool used for storage
-    # therefore we use the one at [0]
-    my $free_space = $json->[0]->{stor_pools}->[0]->{free_space};
-    return undef unless $free_space;
+    # Iterate over "all of them" (usually only one),
+    # because that's effectively what we will get if we create a
+    # new volume with auto-place without specifying the pool name.
+    for my $pool (@{$json->[0]->{stor_pools}}) {
+	$avail += $pool->{free_space}->{free_capacity};
+	$total += $pool->{free_space}->{total_capacity};
+    }
+
+    return undef unless $total;
 
     # they want it in bytes
-    $avail = $free_space->{free_capacity} * 1024;
-    $total = $free_space->{total_capacity} * 1024;
-    $used  = $total - $avail;
-
-    return ( $total, $avail, $used, 1 );
+    $total *= 1024;
+    $avail *= 1024;
+    return ($total, $avail, $total - $avail, 1);
 }
 
 sub activate_storage {
