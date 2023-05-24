LINBIT/linstor-proxmox

commit e3b49eeb52ded60add7f3ef0e5e05a9b6a88ded7

status: show cluster wide view
So far we more or less (size reporting was always a mess) tried to show
a node-local view of storage. For example we only showed pools that
actually exist on a node. This was okay, but had two problems:

- showing only the local storage in a distributed cluster does not make
  too much sense. The actual data lands on nodes LINSTOR decides.
- there are people that have the storage nodes pretty much separated
  from the PVE nodes. For these we did not show any storage information
  on such nodes (as they did not have the RG's SP deployed), which is
  rather confusing.

Now we shifted the calculation of free/used storage to LINSTOR and that
is what we show on all nodes.

tl;tr: we shifted from a node local view to a cluster view.

For PVE nodes we have the following assumption:
- linstor-satellite (and all that comes with it: drbd-utils,
  drbd-dkms,...)
- linstor-proxmox plugin installed

LINBIT/Linstor.pm
@@ -242,7 +242,7 @@ sub create_resource_res_group {
               }
           )
         );
-        print "  Diskfull assignment failed, let's autoplace it.\n"
+        print "  Diskfull assignment on $local_node_name failed, let's autoplace it.\n"
           unless $ret->responseCode() eq '200';
 
         $ret = $self->{cli}->POST( "/v1/resource-definitions/$name/autoplace",
@@ -432,6 +432,20 @@ sub get_storagepool_for_resource_group {
     die $@ if $@;
 
     return $resgroups->{select_filter}->{storage_pool_list};
+}
+
+sub query_size_info {
+	my ( $self, $resgroup_name ) = @_;
+	my $ret = $self->{cli}->POST("/v1/resource-groups/$resgroup_name/query-size-info",
+		encode_json( {} ) );
+	dieContent "Could not query size info for res group $resgroup_name", $ret
+	unless $ret->responseCode() eq '200';
+
+	my $size_info;
+	eval { $size_info = decode_json( $ret->responseContent() ); };
+	die $@ if $@;
+
+	return $size_info->{space_info};
 }
 
 1;

LINSTORPlugin.pm
@@ -376,32 +376,32 @@ sub list_images {
 sub status {
     my ( $class, $storeid, $scfg, $cache ) = @_;
     my $nodename = PVE::INotify::nodename();
+    my $res_grp = get_resource_group($scfg);
 
-    unless($cache->{"linstor:storagepools"}) {
-        my $pools_cache = '/var/cache/linstor-proxmox/pools';
+    my $cache_key = 'linstor:sizeinfo:' . $res_grp;
+    unless($cache->{$cache_key}) {
+        my $info_cache = '/var/cache/linstor-proxmox/sizeinfo:' . $res_grp;
         my $max_age = get_status_cache($scfg);
 
-        if ($max_age and not cache_needs_update($pools_cache, $max_age)) {
-            $cache->{"linstor:storagepools"} = lock_retrieve($pools_cache);
+        if ($max_age and not cache_needs_update($info_cache, $max_age)) {
+            $cache->{$cache_key} = lock_retrieve($info_cache);
         } else {
-            my $sps = linstor($scfg)->get_storagepools();
-            $cache->{"linstor:storagepools"} = $sps;
-            lock_store($sps, $pools_cache) if $max_age;
+            my $infos = linstor($scfg)->query_size_info($res_grp);
+            $cache->{$cache_key} = $infos;
+            lock_store($infos, $info_cache) if $max_age;
         }
     }
 
-    my $storagepools = $cache->{"linstor:storagepools"};
-    my $sp = get_storagepool($scfg);
-
-    my ( $total, $avail ) =
-      LINBIT::PluginHelper::get_status( $storagepools, $sp,
-        $nodename );
+    my  $total = $cache->{$cache_key}->{capacity_in_kib};
+    my  $avail = $cache->{$cache_key}->{available_size_in_kib};
     return undef unless $total;
 
     # they want it in bytes
     $total *= 1024;
     $avail *= 1024;
-    return ( $total, $avail, $total - $avail, 1 );
+    my $used = $total - $avail;
+    $used = 0 if $used < 0; # there was a linstor bug in calculating thin storage, at least don't generate negative value
+    return ( $total, $avail, $used, 1 );
 }
 
 sub activate_storage {

LINBIT/PluginHelper.pm
@@ -48,25 +48,4 @@ sub get_images {
     return $res;
 }
 
-sub get_status {
-    my ( $storage_pools, $storage_pool, $node_name ) = @_;
-    #     overall      , filter      , node_filter
-
-    my ( $avail_kib, $total_kib );
-    foreach my $name ( keys %$storage_pools ) {
-        next if $storage_pool and 0 == ( scalar grep { $_ eq $name } @$storage_pool );
-
-        # skip if not on this node
-        next unless exists $storage_pools->{$name}->{$node_name};
-
-        # skip diskless pools. LINSTOR considers them having infinite space, so they have some MAX values.
-        next if $storage_pools->{$name}->{$node_name}->{conf_as_diskless};
-
-        $avail_kib += $storage_pools->{$name}->{$node_name}->{free_capacity_kib};
-        $total_kib += $storage_pools->{$name}->{$node_name}->{total_capacity_kib};
-    }
-
-    return ( $total_kib, $avail_kib );
-}
-
 1;

commit 4a0c198f9167902750c227d0e9945fcff14e110b

fix performance regression in status calls
This was introduced in e3b49eeb52ded60add7f3ef0e5e05a9b6a88ded7

Long story short, PVE calls status() freaking often. Depending on if you
select the storage in the GUI, this can happen every second, otherwise
like every 10s. To accommodate for that Proxmox's status() API has a
"cache" object that is passed in for every storage type. Let's say you
have 4 'drbd' storages (as in storage.cfg), then for the first call we
get an empty cache which we can fill, and for the next 3 we can use
that.

That helps, but
- we can not make use of that for more than one iteration (remember, PVE
  calls status an awful lot), that is why we introduced a file based
  persistent cache to keep some information longer than 10 seconds.
- things get even worse if you have a distributed system where then
  every PVE node hammers the LINSTOR controller with (the same)
  questions all the time.

Do the math, the LINSTOR controller gets too many "what is the status of
my storage" calls very very quickly even in setups with a handful of
nodes.

Recently we improved how we display storage information, but that also
rendered the built in PVE cache useless as we needed to query every RG
individually. That was known, I thought we could get away with it. We
did not.

How was it fixed:
- we can now query the storage information for all RGs once. That helps
  to again use the builtin cache more effectively as we again need 1
  call for every iteration.
- The new LINSTOR API for querying all RGs at once also got a cache
  parameter which also helps a lot because it avoids the pressure on the
  controller that resulted from the distributed nature of LINSTOR/DRBD.
  The http queries were/are not the problem, the problem is that the
  controller then needs to get the state of all satellites.

LINBIT/Linstor.pm
@@ -448,4 +448,18 @@ sub query_size_info {
 	return $size_info->{space_info};
 }
 
+sub query_all_size_info {
+	my ( $self, $max_age) = @_;
+	my $ret = $self->{cli}->POST("/v1/queries/resource-groups/query-all-size-info",
+		encode_json( {ignore_cache_older_than_sec => $max_age} ) );
+	dieContent "Could not query all size infos for res groups", $ret
+	unless $ret->responseCode() eq '200';
+
+	my $size_info;
+	eval { $size_info = decode_json( $ret->responseContent() ); };
+	die $@ if $@;
+
+	return $size_info->{result};
+}
+
 1;

LINSTORPlugin.pm
@@ -378,22 +378,23 @@ sub status {
     my $nodename = PVE::INotify::nodename();
     my $res_grp = get_resource_group($scfg);
 
-    my $cache_key = 'linstor:sizeinfo:' . $res_grp;
+    my $cache_key = 'linstor:sizeinfos';
     unless($cache->{$cache_key}) {
-        my $info_cache = '/var/cache/linstor-proxmox/sizeinfo:' . $res_grp;
+        my $info_cache = '/var/cache/linstor-proxmox/sizeinfos';
         my $max_age = get_status_cache($scfg);
 
         if ($max_age and not cache_needs_update($info_cache, $max_age)) {
             $cache->{$cache_key} = lock_retrieve($info_cache);
         } else {
-            my $infos = linstor($scfg)->query_size_info($res_grp);
+            # plugin uses 0 for disabled cache, LINSTOR -1, but no actual difference, so...
+            my $infos = linstor($scfg)->query_all_size_info($max_age);
             $cache->{$cache_key} = $infos;
             lock_store($infos, $info_cache) if $max_age;
         }
     }
 
-    my  $total = $cache->{$cache_key}->{capacity_in_kib};
-    my  $avail = $cache->{$cache_key}->{available_size_in_kib};
+    my  $total = $cache->{$cache_key}->{$res_grp}->{space_info}->{capacity_in_kib};
+    my  $avail = $cache->{$cache_key}->{$res_grp}->{space_info}->{available_size_in_kib};
     return undef unless $total;
 
     # they want it in bytes

debian/control
@@ -13,7 +13,7 @@ Depends: drbd-utils,
          libpve-storage-perl,
          librest-client-perl,
          libtypes-serialiser-perl,
-         linstor-satellite (>= 1.21.1),
+         linstor-satellite (>= 1.23.0),
          ${misc:Depends},
          ${perl:Depends}
 Suggests: linstor-controller